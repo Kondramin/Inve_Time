a0c71aa73b020c524b3e014bcadb0a703126396c
Артём Кондратьев <exitway12@gmail.com>
2021-07-13 16:11:09 +0300

    Realise lambda command

---
+++ Inve_Time/Commands/Base/LambdaCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Inve_Time.Commands.Base
+{
+    class LambdaCommand : Command
+    {
+        private readonly Action<object> _Exequte;
+        private readonly Func<object, bool> _CanExequte;
+
+        public LambdaCommand(Action<object> Exequte, Func<object, bool> CanExequte = null)
+        {
+            _Exequte = Exequte ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExequte = CanExequte;
+        }
+
+        public override bool CanExecute(object parameter) => _CanExequte?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _Exequte(parameter);
+    }
+}
