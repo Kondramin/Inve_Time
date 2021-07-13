1dcffb81787e689b126ed1f522c4a8999117f9c8
Артём Кондратьев <exitway12@gmail.com>
2021-07-13 15:50:55 +0300

    Realise Base command and ViewModel

---
+++ Inve_Time/Commands/Base/Command.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Inve_Time.Commands.Base
+{
+    internal abstract class Command : ICommand
+    {
+
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+
+
+        public abstract bool CanExecute(object parameter);
+
+
+        public abstract void Execute(object parameter);
+
+    }
+}
