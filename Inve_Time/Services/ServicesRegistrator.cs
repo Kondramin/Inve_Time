374edb86fb08b01705b4a1fdb2eddbc86316b1ca
Артём Кондратьев <exitway12@gmail.com>
2021-07-15 12:01:04 +0300

    Little fix :)

--- Inve_Time/Services/ServicesRegistrator.cs
+++ Inve_Time/Services/ServicesRegistrator.cs
@@ -8,7 +8,7 @@ namespace Inve_Time.Services
         public static IServiceCollection AddServices(this IServiceCollection services) => services
             .AddTransient<IParserService, ParserService>()
             .AddTransient<IAutoChoseCategoryProductService, AutoChoseCategoryProductService>()
-            .AddTransient<IAutorisationService, AutorisationService>();
+            .AddTransient<IAutorisationService, AutorisationService>()
             ;
     }
 }
