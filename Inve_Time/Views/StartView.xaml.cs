cfda93e8e642a14f4d9f34cb3f30f7e6e3a300f4
Артём Кондратьев <exitway12@gmail.com>
2021-07-17 18:02:15 +0300

    Connect templates app

--- Inve_Time/Views/StartView.xaml.cs
+++ Inve_Time/Views/StartView.xaml.cs
@@ -5,5 +5,10 @@ namespace Inve_Time.Views
     public partial class StartView : UserControl
     {
         public StartView() => InitializeComponent();
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
