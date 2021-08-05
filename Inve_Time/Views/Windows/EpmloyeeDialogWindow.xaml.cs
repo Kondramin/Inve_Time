9dd48548f72796c85c21886db755f7156a97c0f4
Артём Кондратьев <exitway12@gmail.com>
2021-08-05 14:53:04 +0300

    Refactoring

--- Inve_Time/Views/Windows/EpmloyeeDialogWindow.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows;
-
-namespace Inve_Time.Views.Windows
-{
-    public partial class EpmloyeeDialogWindow : Window
-    {
-        public EpmloyeeDialogWindow() => InitializeComponent();
-    }
-}
