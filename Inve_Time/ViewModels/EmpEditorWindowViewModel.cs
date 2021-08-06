6dfa53c04577912d2d696fe837df1146397d80c8
Артём Кондратьев <exitway12@gmail.com>
2021-08-06 16:00:23 +0300

    Working/Refactoring

---
+++ Inve_Time/ViewModels/EmpEditorWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Inve_Time.DataBase.dll.Entities;
+using Inve_Time.ViewModels.Base;
+
+namespace Inve_Time.ViewModels
+{
+    /// <summary>ViewModel of EmpEditorWindow</summary>
+    class EmpEditorWindowViewModel : ViewModel
+    {
+        public EmpEditorWindowViewModel(Employee emp)
+        {
+
+        }
+    }
+}
