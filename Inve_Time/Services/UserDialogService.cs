47e060eeb378180959ebaf8b37d9145c53dd5c00
Артём Кондратьев <exitway12@gmail.com>
2021-08-06 15:55:16 +0300

    Refactoring

--- Inve_Time/Services/UserDialogService.cs
+++ Inve_Time/Services/UserDialogService.cs
@@ -10,27 +10,27 @@ namespace Inve_Time.Services
         public bool Edit(Employee emp)
         {
             //TODO:Refactoring
-            var emp_editor_viewModel = new EpmloyeeEditorWindowViewModel(emp);
+            ////////var emp_editor_viewModel = new EpmloyeeEditorWindowViewModel(emp);
 
            
-            var emp_editor_window = new EpmloyeeEditorWindow
-            {
-                DataContext = emp_editor_viewModel
-            };
+            //////var emp_editor_window = new EpmloyeeEditorWindow
+            //////{
+            //////    DataContext = emp_editor_viewModel
+            //////};
 
             
-            if (emp_editor_window.DialogResult != true) return false;
+            //////if (emp_editor_window.DialogResult != true) return false;
 
 
-            emp.Id = emp_editor_viewModel.EmployeeId;
-            emp.SecondName = emp_editor_viewModel.EmployeeSecondName;
-            emp.Name = emp_editor_viewModel.EmployeeName;
-            emp.Patronymic = emp_editor_viewModel.EmployeePatronymic;
-            emp.Phone = emp_editor_viewModel.EmployeePhone;
-            emp.Email = emp_editor_viewModel.EmployeeEmail;
-            emp.Login = emp_editor_viewModel.EmployeeLogin;
-            emp.Password.Name = emp_editor_viewModel.EmployeePassword;
-            emp.Position.Name = emp_editor_viewModel.EmployeePositionName;
+            //////emp.Id = emp_editor_viewModel.EmployeeId;
+            //////emp.SecondName = emp_editor_viewModel.EmployeeSecondName;
+            //////emp.Name = emp_editor_viewModel.EmployeeName;
+            //////emp.Patronymic = emp_editor_viewModel.EmployeePatronymic;
+            //////emp.Phone = emp_editor_viewModel.EmployeePhone;
+            //////emp.Email = emp_editor_viewModel.EmployeeEmail;
+            //////emp.Login = emp_editor_viewModel.EmployeeLogin;
+            //////emp.Password.Name = emp_editor_viewModel.EmployeePassword;
+            //////emp.Position.Name = emp_editor_viewModel.EmployeePositionName;
 
 
             return true;
---
+++ Inve_Time/Views/Windows/EmpEditorWindow.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace Inve_Time.Views.Windows
+{
+    public partial class EmpEditorWindow : Window
+    {
+        public EmpEditorWindow() => InitializeComponent();
+    }
+}
