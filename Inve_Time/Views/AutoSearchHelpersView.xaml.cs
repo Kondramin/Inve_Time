3324dbb3e26e9798b74eef98a9baccd84436f522
Артём Кондратьев <exitway12@gmail.com>
2021-08-10 15:48:18 +0300

    Refactoring auto cearch categories

---
+++ Inve_Time/Views/AutoSearchHelpersView.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Controls;
+
+namespace Inve_Time.Views
+{
+    public partial class AutoSearchHelpersView : UserControl
+    {
+        public AutoSearchHelpersView() => InitializeComponent();
+    }
+}
