466e64b3dafe673cfa8741fe3145c91fae786143
Артём Кондратьев <exitway12@gmail.com>
2021-08-16 17:53:47 +0300

    add xaml comments, realise notifi MessageBoxes.

--- Inve_Time/Services/ServiceInterfaces/IUserDialog.cs
+++ Inve_Time/Services/ServiceInterfaces/IUserDialog.cs
@@ -5,9 +5,27 @@ namespace Inve_Time.Services.ServiceInterfaces
     /// <summary>Interface of user dialog service</summary>
     interface IUserDialog
     {
-        /// <summary>Edit employee</summary>
+        /// <summary>Show dialog window to edit employee</summary>
         /// <param name="employee">Entity of Employee</param>
         /// <returns>true/false</returns>
-        bool Edit(Employee employee);
+        bool EditEpmloyee(Employee employee);
+
+        /// <summary>Show MessageBox to confirm some action</summary>
+        /// <param name="Information">Information in MessageBox</param>
+        /// <param name="Caption">Caption of MessageBox</param>
+        /// <returns>true/false</returns>
+        bool ConfirmInformation(string Information, string Caption);
+
+        /// <summary>Show MessageBox about warning to user</summary>
+        /// <param name="Warning">Messege of warning</param>
+        /// <param name="Caption">Caption of MessageBox</param>
+        /// <returns>true/false</returns>
+        bool ConfirmWarning(string Warning, string Caption);
+
+        /// <summary>Show MessageBox about error to user</summary>
+        /// <param name="Error">Messege of warning</param>
+        /// <param name="Caption">Caption of MessageBox</param>
+        /// <returns>true/false</returns>
+        bool ConfirmError(string Error, string Caption);
     }
 }
--- Inve_Time/Services/UserDialogService.cs
+++ Inve_Time/Services/UserDialogService.cs
@@ -3,54 +3,79 @@ using Inve_Time.Interfaces.dll;
 using Inve_Time.Services.ServiceInterfaces;
 using Inve_Time.ViewModels;
 using Inve_Time.Views.Windows;
+using System.Windows;
 
 namespace Inve_Time.Services
 {
     class UserDialogService : IUserDialog
     {
-        private readonly IRepository<Position> _PositionRep;
-        private readonly IRepository<Password> _PasswodRep;
+        private readonly IRepository<Position> _PositionRepository;
+        private readonly IRepository<Password> _PasswodRepository;
 
         public UserDialogService(
-            IRepository<Position> PositionRep,
-            IRepository<Password> PasswodRep
+            IRepository<Position> PositionRepository,
+            IRepository<Password> PasswodRepository
             )
         {
-            _PositionRep = PositionRep;
-            _PasswodRep = PasswodRep;
+            _PositionRepository = PositionRepository;
+            _PasswodRepository = PasswodRepository;
         }
 
-        public bool Edit(Employee emp)
+        
+
+        public bool EditEpmloyee(Employee employee)
         {
             //TODO:Refactoring
-            var emp_editor_viewModel = new EmpEditorWindowViewModel(emp, _PositionRep, _PasswodRep);
+            var employee_editor_viewModel = new EmpEditorWindowViewModel(employee, _PositionRepository, _PasswodRepository);
 
 
-            var emp_editor_window = new EmpEditorWindow
+            var employee_editor_window = new EmpEditorWindow
             {
-                DataContext = emp_editor_viewModel,
+                DataContext = employee_editor_viewModel,
                 Height = 450,
                 Width = 380,
                 
             };
 
 
-            if (emp_editor_window.ShowDialog() != true) return false;
+            if (employee_editor_window.ShowDialog() != true) return false;
 
 
-            emp.Id = emp_editor_viewModel.EmpId;
-            emp.SecondName = emp_editor_viewModel.EmpSecondName;
-            emp.Name = emp_editor_viewModel.EmpName;
-            emp.Patronymic = emp_editor_viewModel.EmpPatronymic;
-            emp.Phone = emp_editor_viewModel.EmpPhone;
-            emp.Email = emp_editor_viewModel.EmpEmail;
-            emp.Login = emp_editor_viewModel.EmpLogin;
-            emp.Position = emp_editor_viewModel.SelectedPosition;
-            //emp.Password.Name = emp_editor_viewModel
+            employee.Id = employee_editor_viewModel.EmpId;
+            employee.SecondName = employee_editor_viewModel.EmpSecondName;
+            employee.Name = employee_editor_viewModel.EmpName;
+            employee.Patronymic = employee_editor_viewModel.EmpPatronymic;
+            employee.Phone = employee_editor_viewModel.EmpPhone;
+            employee.Email = employee_editor_viewModel.EmpEmail;
+            employee.Login = employee_editor_viewModel.EmpLogin;
+            employee.Position = employee_editor_viewModel.SelectedPosition;
+            //employee.Password.Name = employee_editor_viewModel
             
-
 
             return true;
         }
+
+
+        public bool ConfirmInformation(string Information, string Caption) => MessageBox.Show(
+            Information, Caption,
+            MessageBoxButton.YesNo,
+            MessageBoxImage.Information)
+            == MessageBoxResult.Yes;
+        
+
+
+        public bool ConfirmWarning(string Warning, string Caption) => MessageBox.Show(
+            Warning, Caption,
+            MessageBoxButton.YesNo,
+            MessageBoxImage.Warning)
+            == MessageBoxResult.Yes;
+
+
+        public bool ConfirmError(string Error, string Caption) => MessageBox.Show(
+            Error, Caption,
+            MessageBoxButton.YesNo,
+            MessageBoxImage.Error)
+            == MessageBoxResult.Yes;
     }
 }
+    
