432dc1dba9099401db175b658de99373fb71f30e
Артём Кондратьев <exitway12@gmail.com>
2021-08-23 16:49:42 +0300

    Add Change password view

---
+++ Inve_Time/Views/Windows/ChangePassword.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace Inve_Time.Views.Windows
+{
+    public partial class ChangePassword : Window
+    {
+        public ChangePassword() => InitializeComponent();
+    }
+}
