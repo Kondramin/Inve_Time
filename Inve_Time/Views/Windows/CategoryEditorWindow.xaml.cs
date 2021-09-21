f0d4223b2cdc47d62b860acfe67072a4d6a67447
Артём Кондратьев <exitway12@gmail.com>
2021-09-21 17:54:47 +0300

    Realised desing to edit category window

---
+++ Inve_Time/Views/Windows/CategoryEditorWindow.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace Inve_Time.Views.Windows
+{
+    public partial class CategoryEditorWindow : Window
+    {
+        public CategoryEditorWindow() => InitializeComponent();
+    }
+}
