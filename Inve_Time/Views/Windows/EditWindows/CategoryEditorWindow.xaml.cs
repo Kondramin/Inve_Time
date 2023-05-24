d598a2122cd7ff0154beaedc555a27331e043462
Kondramin <58448987+Kondramin@users.noreply.github.com>
2023-05-24 14:52:44 +0300

    Delete CategoryEditorWindow.xaml.cs

--- Inve_Time/Views/Windows/EditWindows/CategoryEditorWindow.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows;
-
-namespace Inve_Time.Views.Windows.EditWindows
-{
-    public partial class CategoryEditorWindow : Window
-    {
-        public CategoryEditorWindow() => InitializeComponent();
-    }
-}
